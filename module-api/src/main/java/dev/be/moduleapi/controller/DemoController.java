52154cdd4b5abeeb1a1bb76797d8a378433218c8
choiseon <com1368@naver.com>
2022-12-25 15:16:24 +0900

    feat : 멀티모듈 공부

--- module-api/src/main/java/dev/be/moduleapi/controller/DemoController.java
+++ module-api/src/main/java/dev/be/moduleapi/controller/DemoController.java
@@ -1,6 +1,8 @@
 package dev.be.moduleapi.controller;
 
 import dev.be.moduleapi.service.DemoService;
+import dev.be.modulecommon.domain.Member;
+import dev.be.modulecommon.repositories.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private final DemoService demoService;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/save")
     public String save() {
+        memberRepository.save(Member.builder().name(Thread.currentThread().getName()).build());
         return demoService.save();
     }
 
     @GetMapping("/find")
     public String find() {
+        int size = memberRepository.findAll().size();
+        System.out.println(size);
         return demoService.find();
     }
 
---
+++ module-common/src/main/java/dev/be/modulecommon/domain/Member.java
@@ -0,0 +1,23 @@
+package dev.be.modulecommon.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+}
